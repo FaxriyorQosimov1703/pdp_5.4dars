7f18b8228ce992ec274fe4006f1443ed98acd412
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:05:44 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Users from './Components/Users'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    state = {
+        user: {
+            fistname: 'faxriyor',
+            lastname: 'qosimov',
+            balance: 1000
+        }
+    }
+
+    Addcount=()=> {
+        let current = this.state.user;
+        current.balance+=1000
+        this.setState({
+            user: current 
+        })
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className="row bg-dark text-white p-4">
+                    <div className="col-12 bg-info">
+                         <Users user={this.state.user}/>
+                    </div>
+                </div>
+               <button className="btn btn-danger" onClick={this.Addcount}>salomalekum</button>
+            </div>
+        )
+    }
 }
-
-export default App;
